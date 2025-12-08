2f744352248e65a58e2fa4dda1841df905c84cf2
thanhtrung123777 <thtrung211@gmail.com>
2025-12-08 22:04:05 +0700

    xong home

--- js/index.js
+++ js/index.js
@@ -39,3 +39,29 @@ left.addEventListener('click', function () {
     }
 })
 
+
+const btns = document.querySelectorAll('.topic div')
+let img = document.querySelector('.show-pics img')
+let topic = document.querySelector('.introduce-room h2')
+let para = document.querySelector('.introduce-room p')
+btns[0].addEventListener('click', function () {
+    img.src = 'img/Capell-Bangkok-Riverfront-011.png'
+    topic.textContent = 'ROOM'
+    para.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.'
+    document.querySelector('.hr').classList.remove('hr')
+    document.querySelector('.btn1').classList.add('hr')
+})
+btns[1].addEventListener('click', function () {
+    img.src = 'img/Capella-Bangkok-Courtyard-Suite-g01 1.png'
+    topic.textContent = 'SUITES'
+    para.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.'
+    document.querySelector('.hr').classList.remove('hr')
+    document.querySelector('.btn2').classList.add('hr')
+})
+btns[2].addEventListener('click', function () {
+    img.src = 'img/Capella-Bangkok-Villa-01a 1.png'
+    topic.textContent = 'VILLA'
+    para.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.'
+    document.querySelector('.hr').classList.remove('hr')
+    document.querySelector('.btn3').classList.add('hr')
+})
