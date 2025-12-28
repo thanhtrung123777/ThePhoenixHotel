d14d574a334f18e71e6100d4750af36c06e35e3f
thanhtrung123777 <thtrung211@gmail.com>
2025-12-28 23:43:06 +0700

    res ex 1

--- js/experiences.js
+++ js/experiences.js
@@ -32,3 +32,13 @@ left.addEventListener('click', function () {
 })
 
 
+const burger = document.querySelector('.burger');
+const menu = document.querySelector('.menu');
+
+burger.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
+
+
+
+
