d93b74e4a33313c52d593786b28cea055c07976b
thanhtrung123777 <thtrung211@gmail.com>
2025-12-29 00:46:50 +0700

    4

--- js/experiences.js
+++ js/experiences.js
@@ -33,10 +33,13 @@ left.addEventListener('click', function () {
 
 
 const burger = document.querySelector('.burger');
-const menu = document.querySelector('.menu');
-
+const menu = document.querySelector('.sticky-header');
 burger.addEventListener('click', () => {
-    menu.classList.toggle('active');
+    if (menu.classList.contains('active')) {
+        menu.classList.remove('active'); // đóng
+    } else {
+        menu.classList.add('active'); // mở
+    }
 });
 
 
